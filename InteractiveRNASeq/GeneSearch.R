a538dc9e2b1fe3263417bbfbf7779967b3c04237
Jack Bruton <jackobruton@hotmail.co.uk>
2022-09-02 15:26:40 +0100

    Began work on database manipulation
    added 3 variables to store each tissue database. added functionality to remove genes that user defines as not interested in.

--- InteractiveRNASeq/GeneSearch.R
+++ InteractiveRNASeq/GeneSearch.R
@@ -7,15 +7,25 @@ library(ComplexHeatmap)
 library(tidyverse)
 library(readxl)
 
-#Read in raw log fold change database
-
-wdLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 1)
-sgLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 2)
-bLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 3)
-
-
 #VARIABLES TO SET#
-geneList <- c("gene1", "gene2", "geneN") #A list of genes to generate a heatmap for.
+geneList <- c("FBgn0015399", "FBgn0033395", "FBgn0026562") #A list of genes to generate a heatmap for.
 tissuesForHeatmap <- c("Wingdisc", "Salivarygland", "Brain") #The tissues to include in the heatmap.
 genotypesForAnalysis <- c("PtcG4_D6", "Yw_D5", "RasYki_D5", "RasYki_D8", "Fer12OG_D6", "Fer12OG_D8", "Fer12WT_D6","ImpL2D6", "ImpL2D8") #The genotypes to include in the heatmap.
 
+
+#Read in raw log fold change database
+wdLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 1)
+wdLfcTable <- column_to_rownames(wdLfcTable, var = "GeneID")
+
+sgLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 2)
+sgLfcTable <- column_to_rownames(sgLfcTable, var = "GeneID")
+
+bLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 3)
+bLfcTable <- column_to_rownames(bLfcTable, var = "GeneID")
+
+#Filter datasets to only include rows that user has specified in "genelist" variable.
+wdLfcTable <- wdLfcTable[rownames(wdLfcTable) %in% geneList, ]
+sgLfcTable <- sgLfcTable[rownames(sgLfcTable) %in% geneList, ]
+bLfcTable <- bLfcTable[rownames(bLfcTable) %in% geneList, ]
+
+#Remove unwanted tissue datasets#
