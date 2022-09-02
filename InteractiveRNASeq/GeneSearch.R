a519f1fdfefe3f3be12dd6d3a1c11eddf9cf98b1
Jack Bruton <jackobruton@hotmail.co.uk>
2022-09-02 15:01:55 +0100

    Reformatted Layout

--- InteractiveRNASeq/GeneSearch.R
+++ InteractiveRNASeq/GeneSearch.R
@@ -5,12 +5,17 @@
 #Libraries#
 library(ComplexHeatmap)
 library(tidyverse)
-
+library(readxl)
 
 #Read in raw log fold change database
-rawDataTable <- read.csv("GeneSearch_LFC_Database", header = TRUE, sep = ",", row.names = 1)
+
+wdLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 1)
+sgLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 2)
+bLfcTable <- read_excel("All_Tissues_LFC_Database.xlsx", sheet = 3)
+
 
 #VARIABLES TO SET#
 geneList <- c("gene1", "gene2", "geneN") #A list of genes to generate a heatmap for.
-tissuesForHeatmap <- c("Wingdisc", "Salivarygland", "Brain")
-genotypesForAnalysis <- c("PtcG4_D6", "Yw_D5", "RasYki_D5", "RasYki_D8", "Fer12OG_D6", "Fer12OG_D8", "Fer12WT_D6","ImpL2D6", "ImpL2D8")
+tissuesForHeatmap <- c("Wingdisc", "Salivarygland", "Brain") #The tissues to include in the heatmap.
+genotypesForAnalysis <- c("PtcG4_D6", "Yw_D5", "RasYki_D5", "RasYki_D8", "Fer12OG_D6", "Fer12OG_D8", "Fer12WT_D6","ImpL2D6", "ImpL2D8") #The genotypes to include in the heatmap.
+
