284b5d3ba01e1d8607b89382f413513cdfb49cd2
Jack Bruton <jackobruton@hotmail.co.uk>
2022-09-13 14:59:15 +0100

    Finished backend heatmap generator
    - Added function to add/remove genotypes
    -Added function to  add/remove tissues
    - Automatically creates heatmaps that include/exclude above criteria

--- InteractiveRNASeq/GeneSearch.R
+++ InteractiveRNASeq/GeneSearch.R
@@ -6,11 +6,12 @@
 library(ComplexHeatmap)
 library(tidyverse)
 library(readxl)
+library(circlize)
 
 #VARIABLES TO SET#
 geneList <- c("FBgn0015399", "FBgn0033395", "FBgn0026562") #A list of genes to generate a heatmap for.
 tissuesForHeatmap <- c("Wingdisc", "Salivarygland", "Brain") #The tissues to include in the heatmap.
-genotypesForAnalysis <- c("PtcG4_D6", "Yw_D5", "RasYki_D5", "RasYki_D8", "Fer12OG_D6", "Fer12OG_D8", "Fer12WT_D6","ImpL2D6", "ImpL2D8") #The genotypes to include in the heatmap.
+genotypesForAnalysis <- c("PtcG4_D6", "Yw_D5", "RasYki_D5", "RasYki_D8", "Fer12OG_D6", "Fer12OG_D8", "Fer12WT_D6") #The genotypes to include in the heatmap.
 
 
 #Read in raw log fold change database
@@ -29,3 +30,55 @@ sgLfcTable <- sgLfcTable[rownames(sgLfcTable) %in% geneList, ]
 bLfcTable <- bLfcTable[rownames(bLfcTable) %in% geneList, ]
 
 #Remove unwanted tissue datasets#
+genotypesForAnalysis <- append(genotypesForAnalysis, "LFC")
+wdLfcTable <- wdLfcTable[, colnames(wdLfcTable) %in% genotypesForAnalysis]
+sgLfcTable <- sgLfcTable[, colnames(sgLfcTable) %in% genotypesForAnalysis]
+bLfcTable <- bLfcTable[, colnames(bLfcTable) %in% genotypesForAnalysis]
+
+#Select Which tissues to add to heatmap#
+Wingdisc <- wdLfcTable
+Salivarygland <- sgLfcTable
+Brain <- bLfcTable
+
+#Create Heatmap Color Scale.
+colorScale <- colorRamp2(c(-3,0,3), c("blue", "white", "red"))
+
+##HEATMAP GENERATOR LOOP##
+heatmapList <- list()
+for (tissue in tissuesForHeatmap) {
+  
+  #Create normalised count level annotation.
+  ncValues <- get(as.name(tissue))[, colnames(get(as.name(tissue))) == "LFC"]
+  rowAnnotation <- rowAnnotation(LFC = anno_barplot(ncValues), border = TRUE)
+  
+  #Create Core Heatmap database.
+  coreHeatmap <- get(as.name(tissue))[, colnames(get(as.name(tissue))) != "LFC"]
+  
+  #Plot heatmap.
+  heatmap <- Heatmap(as.matrix(coreHeatmap),
+                                            col = colorScale,
+                                            column_title = tissue,
+                                            row_title = "Gene",
+                                            row_title_gp = gpar(col = "white"),
+                                            cluster_columns = FALSE,
+                                            column_title_side = "top",
+                                            row_title_side = "right",
+                                            name = "Log2Fold Change",
+                                            row_gap = unit(1, "mm"),
+                                            border = TRUE,
+                                            right_annotation = rowAnnotation,
+                                            column_names_rot = 90)
+heatmapList <- append(heatmap, heatmapList)
+}
+
+#Plot Heat map Based on Tissues Provided.
+HeatmapListLength <- length(heatmapList)
+
+if(HeatmapListLength == 1) {
+  plot(heatmapList[[1]])
+} else if (HeatmapListLength == 2) {
+  plot(heatmapList[[2]] + heatmapList[[1]])
+} else {
+  plot(heatmapList[[3]] + heatmapList[[2]] + heatmapList[[1]])
+}
+
